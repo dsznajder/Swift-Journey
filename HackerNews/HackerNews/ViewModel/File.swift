db3d5ba5fce91984d96c13a755ac6d193e9cdec7
Damian Sznajder <damsznajder@gmail.com>
2021-02-16 10:55:29 +0100

    Remove trailing files

--- HackerNews/HackerNews/ViewModel/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  HackerNews
-//
-//  Created by Damian Sznajder on 16/02/2021.
-//
-
-import Foundation
--- WeatherApp/WeatherApp/Model/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  WeatherApp
-//
-//  Created by Damian Sznajder on 15/02/2021.
-//
-
-import Foundation
