b82d1f3ea4d171ffeb9b5e21e22cbc99dafc8ed7
Damian Sznajder <damsznajder@gmail.com>
2021-02-16 08:12:18 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Damian Sznajder on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
