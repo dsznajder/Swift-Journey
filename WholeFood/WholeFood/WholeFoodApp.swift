3a7bd1a3349da9465be053e8bd19473767f6d15a
Damian Sznajder <damsznajder@gmail.com>
2021-02-16 10:16:17 +0100

    Init WholeFood

---
+++ WholeFood/WholeFood/WholeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  WholeFoodApp.swift
+//  WholeFood
+//
+//  Created by Damian Sznajder on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct WholeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
